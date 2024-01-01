831bd4428f0721d892d6bcaea6220ed71f0b8eaf
heon <heon2908@gmail.com>
2024-01-01 19:37:16 +0900

    first commit

---
+++ src/test/java/com/example/spring_boot_redis/SpringBootRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
